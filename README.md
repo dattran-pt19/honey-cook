60051d871cfa1165ee3fbf27dbc776cd3c56c148
Trần Đạt <dattran.pt19@gmail.com>
2022-05-24 00:32:01 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# honey_cook
+
+A new Flutter project. I make a app for my honey help she save her dish data.
---
+++ android/app/src/main/kotlin/com/dattran/honey_cook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dattran.honey_cook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
